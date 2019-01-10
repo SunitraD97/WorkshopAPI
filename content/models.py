978526f0009f37ab2c40d4ccddfcfc93362e7f3e
sunitra <sunitra1525@gmail.com>
2019-01-10 10:12:25 +0700

    -edit2

--- content/models.py
+++ content/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     email = models.EmailField()
-    image = models.ImageField()
+    image = models.FileField(upload_to='post_image',blank=True)
     birthdate = models.DateField(blank=True,null=True)
     date_create = models.DateField(blank=True)
     date_update = models.DateField(blank=True)
